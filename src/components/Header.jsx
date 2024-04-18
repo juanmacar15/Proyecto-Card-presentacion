8c1bb0524774e6522a86f18264771ce4fda5a849
juanmacar15 <jdmadrid19@misena.edu.co>
2024-04-18 18:49:25 -0500

    Ajustes para crear mi perfil verdadero, no usar este commit :) a menos que quieras crear el tuyo :)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import img from "../assets/images/avatar-jessica.jpeg"
+import img from "../assets/images/juanmacar.jpg"
 import { References } from './References'
 
 export const Header = () => {
@@ -6,8 +6,8 @@ export const Header = () => {
         <main className="bg-DarkGrey w-[326px] sm:w-[386px] m-auto mt-[116px] sm:mt-[116px] rounded-xl">
             <div className=" flex flex-col items-center p-[40px] ">
                 <img className=" w-[92px] rounded-full" src={img} alt="Image_profile" />
-                <p className="text-White text-[22px] font-bold">Jessica Randall</p>
-                <p className="text-Green text-[12px] font-bold leading-7 ">London, United Kingdom</p>
+                <p className="text-White text-[22px] font-bold">Juan David Madrid</p>
+                <p className="text-Green text-[12px] font-bold leading-7 ">Curumani - Cesar - Colombia</p>
                 < References />
             </div>
         </main>
--- src/components/References.jsx
+++ src/components/References.jsx
@@ -3,12 +3,20 @@ import { Header } from "./Header";
 export const References = () => {
     return (
         <div className=" w-full ">
-            <p className=" text-White text-[12px] text-center">"Front-end developer and avid reader."</p>
+            <p className=" text-White text-[12px] text-center">"El código funciona, no sé por qué"</p>
             <div className="text-center">
-                <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton"> <a href={Header} target="_blank">Frontend Mentor</a></p>
-                <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton"> <a href={Header} target="_blank">Linkedin</a></p>
-                <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton"> <a href={Header} target="_blank">Twitter</a></p>
-                <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton"> <a href={Header} target="_blank">Instagram</a></p>
+                <a href="https://www.instagram.com/juanmacar15/" target="_blank">
+                    <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton">Instagram</p>
+                </a>
+                <a href="https://github.com/juanmacar15" target="_blank">
+                    <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton">GitHub</p>
+                </a>
+                <a href="https://twitter.com/juanmacar_" target="_blank">
+                    <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton">Twitter</p>
+                </a>
+                <a href="https://www.linkedin.com/in/juanmacar/" target="_blank">
+                    <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton">Linkedin</p>
+                </a>
             </div>
 
         </div>
