35a0f937b289067840dfff2031c36f7b7d8687e8
juanmacar15 <jdmadrid19@misena.edu.co>
2024-04-18 19:02:20 -0500

    cambio de logo, cambio de icono de busqueda y cambio de twitter a X :/

--- src/components/References.jsx
+++ src/components/References.jsx
@@ -8,12 +8,13 @@ export const References = () => {
                 <a href="https://www.instagram.com/juanmacar15/" target="_blank">
                     <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton">Instagram</p>
                 </a>
+                <a href="https://twitter.com/juanmacar_" target="_blank">
+                    <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton">X</p>
+                </a>
                 <a href="https://github.com/juanmacar15" target="_blank">
                     <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton">GitHub</p>
                 </a>
-                <a href="https://twitter.com/juanmacar_" target="_blank">
-                    <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton">Twitter</p>
-                </a>
+                
                 <a href="https://www.linkedin.com/in/juanmacar/" target="_blank">
                     <p className="py-[8px] my-[10px] rounded-md bg-GrayButton cursor-pointer text-White font-bold text-[12px] hover:bg-Green hover:text-GrayButton">Linkedin</p>
                 </a>
